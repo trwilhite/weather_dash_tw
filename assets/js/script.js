1b6483fec3a6821f724c9ade7543688c2bb8eff9
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-01-06 22:13:17 -0500

    changed html again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var currentContainerEl = document.getElementById("current");
+
 function searchCity () {
     var cityInput = document.getElementById("cityInput").value.trim();
     console.log(cityInput);
@@ -15,16 +17,17 @@ function currentWeather(city) {
             console.log(response);
             response.json().then(function(data) {
                 console.log(data);
+                displayCurrent(data, city);
                 uviForecast(data.coord.lat, data.coord.lon);
             });
         } else {
             alert("City Not Found. Please Search a Valid City Name.")
         }
-        
     });
 };
 
 function uviForecast (lat, lon) {
+    //format oneweather one call api url
     var onecallApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=c12650a5a26dc020183a9dff69826eed";
 
     // make a get request to url
@@ -40,6 +43,34 @@ function uviForecast (lat, lon) {
         }
         
     });
+}
+
+function displayCurrent (cityInput) {
+    // var iconUrl = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+    // var currentDate = data.dt * 1000;
+
+    var currentContainer = $("<div></div>");
+    var currentBody = $("<div></div>");
+    var currentHeader = $("<h3></h3>").text("hello")
+    // var currentTemp = $("<p></p>");
+    // var currentWind = $("<p></p>");
+    // var currentHumidity = $("<p></p>");
+    // var currentUvi = $("<p></p>");
+
+    $(currentContainer).attr("class", "card");
+    $(currentBody).attr("class", "card-body");
+    $(currentBody).attr("id", "currentBody");
+    $(currentHeader).attr("class", "card-title");
+    // $(currentTemp).attr("class", "card-text");
+    // $(currentWind).attr("class", "card-text");
+    // $(currentHumidity).attr("class", "card-text");
+    // $(currentUvi).attr("class", "card-text");
+
+    // $(currentHeader).text(cityInput, currentDate, iconUrl)
+
+    currentContainer.append(currentBody);
+    currentBody.append(currentHeader);
+    currentContainerEl.append(currentContainer);
 }
 
 $("#searchBtn").on("click", searchCity);
